97aeb4d86033cc0b8543dc8af3a603d9409d5d28
NeelHaval <s2634326@ed.ac.uk>
2025-08-05 21:20:32 +0100

    Assembler commit

--- projects/6/Assembler.py
+++ projects/6/Assembler.py
@@ -1,4 +1,4 @@
-# Create dictionary to store predefined symbols and programmer defined labels and symbols
+# Create dictionary to store predefined symbols and programmer defined labels and symbols (symbol table)
 symbolTable = {
 
     "R0": "0",
@@ -25,6 +25,67 @@ symbolTable = {
     "THAT": "4",
     "SCREEN": "16384",
     "KBD": "24576"
+    
+}
+
+# Create dictionaries to store predefined C-instruction operations
+
+# C-instruction comp
+comp = {
+    "0": "101010",
+    "1": "111111",
+    "-1": "111010",
+    "D": "001100",
+    "A": "110000",
+    "!D": "001101",
+    "!A": "110001",
+    "-D": "001111",
+    "-A": "110011",
+    "D+1": "011111",
+    "A+1": "110111",
+    "D-1": "001110",
+    "A-1": "110010",
+    "D+A": "000010",
+    "D-A": "010011",
+    "A-D": "000111",
+    "D&A": "000000",
+    "D|A": "010101",
+    "M": "110000",
+    "!M": "110001",
+    "-M": "110011",
+    "M+1": "110111",
+    "M-1": "110010",
+    "D+M": "000010",
+    "D-M": "010011",
+    "M-D": "000111",
+    "D&M": "000000",
+    "D|M": "010101"
+}
+
+# C-instruction dest
+
+dest = {
+    "null": "000",
+    "M": "001",
+    "D": "010",
+    "DM": "011",
+    "A": "100",
+    "AM": "101",
+    "AD": "110",
+    "ADM": "111"
+}
+
+# C-instruction jump
+
+jump = {
+    "null": "000",
+    "JGT": "001",
+    "JEQ": "010",
+    "JGE": "011",
+    "JLT": "100",
+    "JNE": "101",
+    "JLE": "110",
+    "JMP": "111"
 }
 
 # Prompt user for the assembly file to be translated
@@ -50,13 +111,122 @@ asmFile = open(f"projects/6/{folder}/{userFile}", "r")
 asmCode = asmFile.readlines()
 
 # Remove all white space and comments from assembly code in place
-for index in range(len(asmCode)):
-    line = asmCode[index].strip()
+for asmLine in range(len(asmCode)):
+    line = asmCode[asmLine].strip()
     if "//" in line:
         line = line[:line.index("//")].strip()
-    asmCode[index] = line
+    asmCode[asmLine] = line
 
 # At this point, if line is not empty it should be appended to asmClean
 for line in asmCode:
     if line.strip() != "":
         asmClean.append(line.strip())
+
+# Close the .asm file
+asmFile.close()
+
+# Function to process first pass
+def passOne():
+    
+    # Assigning instruction increment variable
+    instrInc = 0
+
+    # For loop to iterate over asmClean
+    for element in range(len(asmClean)):
+
+        # Assigning line variable
+        line = asmClean[element].strip()
+
+        # If block for label check
+        if "(" in line and ")" in line:
+            labelStart = line.index("(")
+            labelEnd = line.index(")")
+            label = line[labelStart + 1:labelEnd].strip()
+            symbolTable[label] = instrInc
+        
+        # Else block for otherwise increment variable
+        else:
+            instrInc += 1
+
+# Call passOne()
+passOne()
+
+print(symbolTable)
+
+# Create output file
+with open("projects/6/Prog.hack", "w") as hackOut:
+    pass
+
+# Function to process second pass
+def passTwo():
+
+    # Assigning new variable RAM[16]
+    currentRam = 16
+
+    # For loop to iterate over asmClean
+    for element in range(len(asmClean)):
+
+        # Assigning line variable
+        line = asmClean[element].strip()
+
+        # Determine if instruction is A-type
+        aInstr = None
+        if "@" in line:
+            position = line.find("@")
+            aInstr = line[position + 1:].strip()
+
+        # If A-type instruction positive, parse to output binary file
+        if aInstr is not None:
+
+            # If present in symbol table then compute binary result and parse
+            if aInstr in symbolTable:
+                with open("projects/6/Prog.hack", "a") as hackOut:
+                    hackOut.write(f"{format(int(symbolTable[aInstr]), '016b')}")
+
+            # Else block for checking whether constant or new variable
+            else:
+
+                # If constant then parse immediately
+                if aInstr.isdigit():
+                    with open("projects/6/Prog.hack", "a") as hackOut:
+                        hackOut.write(f"{format(int(aInstr), '016b')}")
+
+                # Else block to use in assumption of new variable
+                else:
+                    symbolTable[aInstr] = currentRam
+                    with open("projects/6/Prog.hack", "a") as hackOut:
+                        hackOut.write(f"{format(currentRam, '016b')}")
+                    currentRam += 1
+        
+        # If instruction is not A-type, assume C-type
+        if "(" not in line and ")" not in line:
+            
+            # For Dest=Comp;Jump format
+            if "=" in line and ";" in line:
+                destinationBits = line.split("=")
+                compJumpBits = destinationBits[1].split(";")
+                aBit = "1" if "M" in compJumpBits[0] else "0"
+                with open("projects/6/Prog.hack", "a") as hackOut:
+                    hackOut.write("111" + aBit + comp[compJumpBits[0].strip()] + dest[destinationBits[0].strip()] + jump[compJumpBits[1].strip()])
+
+            # For Dest=Comp format
+            elif "=" in line:
+                tempList1 = line.split("=")
+                aBit = "1" if "M" in tempList1[1] else "0"
+                with open("projects/6/Prog.hack", "a") as hackOut:
+                    hackOut.write("111" + aBit + comp[tempList1[1].strip()] + dest[tempList1[0].strip()] + "000")
+
+            # For Jump format
+            elif ";" in line:
+                tempList2 = line.split(";")
+                aBit = "1" if "M" in tempList2[0] else "0"
+                with open("projects/6/Prog.hack", "a") as hackOut:
+                    hackOut.write("111" + aBit + comp[tempList2[0].strip()] + "000" + jump[tempList2[1].strip()])
+
+        # If on last instruction or label don't add new line
+        if element != len(asmClean) - 1 and "(" not in line:
+            with open("projects/6/Prog.hack", "a") as hackOut:
+                    hackOut.write("\n")
+
+# Call passTwo()
+passTwo()
