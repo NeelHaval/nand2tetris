7ce605b5111155e148198f693862524137ec3957
NeelHaval <s2634326@ed.ac.uk>
2025-08-18 15:01:36 +0100

    Project 8 VMTranslator java files created. Awaiting modifications.

---
+++ projects/8/jvm/src/Parser.java
@@ -0,0 +1,27 @@
+// Package location
+package src;
+
+// Imports
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Create Parser Class
+public class Parser {
+
+    // Define parse() method
+    public static ArrayList<String> parse(String instr) {
+
+        // Return single instruction as an ArrayList<>
+        ArrayList<String> subList = new ArrayList<>(Arrays.asList(instr.split(" ")));
+
+        for (int i = 0; i < subList.size(); i++) {
+
+            subList.set(i, subList.get(i).trim());
+
+        }
+
+        return subList;
+
+    }
+
+}
---
+++ projects/8/jvm/src/VMTranslator.java
@@ -0,0 +1,158 @@
+// Package location
+package src;
+
+// Imports
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Create VMTranslator class
+public class VMTranslator {
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Prompt user for file
+        String fileName = args[0];
+
+        // Send fileName variable to CodeWriter class
+        CodeWriter.fName(fileName.substring(0, fileName.length() - 3));
+
+        // *** TRIAL ***
+        System.out.println("VM file to be translated: " + fileName);
+
+        // Initialize folder
+        String folderName;
+
+        // Compute folder name
+        if (fileName.contains("Basic") || fileName.contains("Pointer") || fileName.contains("Static")) {
+
+            folderName = "MemoryAccess";
+
+        } else if (fileName.contains("Simple") || fileName.contains("Stack")) {
+
+            folderName = "StackArithmetic";
+
+        } else {
+
+            throw new IllegalArgumentException("Please Enter valid file name.");
+
+        }
+
+        // *** TRIAL ***
+        System.out.println("MemoryAccess or StackArithmetic? " + folderName);
+
+        // Initialize sub folders
+        String subFolderName;
+
+        // Compute sub folder name
+        if (fileName.contains("Basic")) {
+
+            subFolderName = "BasicTest";
+
+        } else if (fileName.contains("Pointer")) {
+
+            subFolderName = "PointerTest";
+            
+        } else if (fileName.contains("Static")) {
+
+            subFolderName = "StaticTest";
+            
+        } else if (fileName.contains("Simple")) {
+
+            subFolderName = "SimpleAdd";
+            
+        } else if (fileName.contains("Stack")) {
+
+            subFolderName = "StackTest";
+            
+        } else {
+
+            throw new IllegalArgumentException("Please Enter valid file name.");
+
+        }
+
+        // *** TRIAL ***
+        System.out.println("Sub-Folder: " + subFolderName);
+
+        // Create fileCopyOne
+        ArrayList<String> fileCopy = new ArrayList<>();
+
+        // Try statement
+        try {
+
+            // Create Scanner object
+            Scanner scanner = new Scanner(new File("../" + folderName + "/" + subFolderName + "/" + fileName));
+
+            // Add all lines to fileCopyOne ArrayList
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine().trim();
+                fileCopy.add(line);
+
+            }
+
+            scanner.close();
+
+        // Catch statement
+        } catch (FileNotFoundException err) {
+
+            System.out.println("Please enter valid file.");
+
+        }
+
+        // Remove comments by looping backwards
+        for (int i = fileCopy.size() - 1; i >= 0; i--) {
+
+          if (fileCopy.get(i).contains("//")) {
+
+            int position = fileCopy.get(i).indexOf("//");
+            String instrOnly = ((fileCopy.get(i)).substring(0, position)).trim();
+            fileCopy.set(i, instrOnly);
+
+          }  
+
+        }
+
+        // Remove empty lines by looping backwards
+        for (int i = fileCopy.size() - 1; i >= 0; i--) {
+
+            if (fileCopy.get(i).isEmpty()) {
+
+                fileCopy.remove(i);
+
+            }
+
+        }
+
+        // *** TRIAL ***
+        System.out.println("Current state of VM instructions to be parsed: " + fileCopy);
+
+        // Open file and erase
+        try (FileWriter asm = new FileWriter(fileName.substring(0, fileName.length() - 3) + ".asm", false);) {
+
+            asm.write("");
+
+        } catch (IOException err) {
+
+            err.printStackTrace();
+
+        }
+
+        // Pass each instruction to Parser class
+        for (int i = 0; i < fileCopy.size(); i++) {
+
+            // Retrieve instructions and use parse() method from Parser class
+            ArrayList<String> instrArrList = Parser.parse(fileCopy.get(i));
+
+            // Use writeAsm() from CodeWriter class to write assembly
+            CodeWriter.writeAsm(instrArrList);
+
+        }
+
+    }
+
+}
